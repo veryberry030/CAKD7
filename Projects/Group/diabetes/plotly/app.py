a3c22c94606bb100591d671cb2ed2fc1649a5e08
AI.chemist <junghyun320@naver.com>
2022-09-15 17:52:43 +0900

    0915

---
+++ Projects/Group/diabetes/plotly/app.py
@@ -0,0 +1,40 @@
+import dash
+from dash import dcc
+from dash import html
+from flask import Flask
+import plotly.express as px
+import pandas as pd
+
+app = dash.Dash(__name__)
+
+df = pd.DataFrame(
+    {
+        "Fruit": [
+            "Apples",
+            "Oranges",
+            "Bananas",
+            "Apples",
+            "Oranges",
+            "Bananas",
+        ],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
+    }
+)
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(
+    children=[
+        html.H1(children='Hello Dash'),
+        html.Div(
+            children='''
+        Dash: A web application framework for your data.
+    '''
+        ),
+        dcc.Graph(id='example-graph', figure=fig),
+    ]
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
